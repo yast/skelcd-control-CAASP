1d81c58a273a21161acb7d31542817207b1f8625
Ladislav Slezák <lslezak@suse.cz>
2017-09-18 20:34:40 +0200

    Generate Kubic control.xml using XSL transformation
    - That's more reliable than plain text patching
    - Easier to update
    - Build both (Kubic and CaaSP) flavors in Travis
    - The hack in `.gitignore` is not needed anymore
    - 15.0.2

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,20 @@
 
 set -e -x
 
-# validate the control file
+if [ "$BUILD_FLAVOR" == "kubic" ]; then
+  # build the file
+  make -C control control.Kubic.xml
+  # pretend we are running in an openSUSE Kubic build
+  echo "%is_susecaasp 0" >> ~/.rpmmacros
+elif [ "$BUILD_FLAVOR" == "caasp" ]; then
+  # pretend we are running in a SUSE CaaSP build
+  echo "%is_susecaasp 1" >> ~/.rpmmacros
+else
+  echo "Uknown BUILD_FLAVOR: $BUILD_FLAVOR"
+  exit 1
+fi
+
+# validate the control file(s)
 make -C control check
 
 # build the package
